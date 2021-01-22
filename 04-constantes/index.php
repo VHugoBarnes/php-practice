690273a57f3b8c8412402cd0a0c9986e5797def1
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-21 21:36:19 -0600

    Constantes predefinidas

--- 04-constantes/index.php
+++ 04-constantes/index.php
@@ -6,6 +6,16 @@ define('nombre', 'Víctor Hugo');
 define('apellidos', 'Vázquez Gómez');
 
 echo nombre;
-echo apellidos
+echo apellidos;
+
+// Constantes predefinidas
+echo PHP_OS;
+echo PHP_VERSION;
+echo PHP_EXTENSION_DIR;
+echo __LINE__;
+echo __FILE__;
+echo __FUNCTION__;
+echo __CLASS__;
+echo __NAMESPACE__;
 
 ?>
