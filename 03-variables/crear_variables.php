22a5314860924d3e41abbb007ee634ceba6363f9
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-21 21:12:45 -0600

    Mas variables

--- 03-variables/crear_variables.php
+++ 03-variables/crear_variables.php
@@ -2,6 +2,12 @@
 
 // Creando una variable
 $mi_primera_variable = 'Hola mundo';
+$edad = 21;
+$keko_es_bonito = true;
+
+// Sobreescritura de variables
+$mi_primera_variable = 'Hola Keko';
+
 // Imprimiendola
 echo '<h1>' .$mi_primera_variable . '</h1>';
 
