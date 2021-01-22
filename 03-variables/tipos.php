f8d74359a6f083d92833e299f59c4ab33209f77a
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-21 21:19:56 -0600

    Tipos de datos en PHP

---
+++ 03-variables/tipos.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Tipos de datos en PHP 
+ * 
+ * Entero: int
+ * Coma flotante: float, double
+ * Cadenas de caracteres: string
+ * booleano: bool
+ * null
+ * Array
+ * Objetos
+ * 
+ */
+
+$numero = 100;
+$decimal = 23.1;
+$texto = 'Soy un texto';
+$verdadero = true;
+$nula = null;
+echo gettype($numero);
+echo gettype($decimal);
+echo gettype($texto);
+echo gettype($verdadero);
+echo gettype($nula);
+
+
+?>
