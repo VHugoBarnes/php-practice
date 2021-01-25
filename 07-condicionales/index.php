d9f49deec96938be790cde9292dd6c24288b7472
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-24 23:03:52 -0600

    Condicional if

---
+++ 07-condicionales/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Operadores de comparación
+// == igual
+// === identico
+// != diferente
+// <> diferente
+// !== no identico
+// < menor que
+// > mayor que
+// <= menor o igual que
+// >= mayor o igual que
+
+$color = "verde";
+
+if($color == "rojo"){
+    echo "el color es rojo";
+} else {
+    echo "el color no es rojo";
+}
+
+?>
