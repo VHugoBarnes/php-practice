3394b8f1107aec55a1abacb2afb67531a97f3c56
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-24 23:37:12 -0600

    Switch

--- 07-condicionales/index.php
+++ 07-condicionales/index.php
@@ -72,4 +72,29 @@ if($pais == 'Mexico' || $pais == 'España' || $pais == 'Argentina') {
     echo "En este país se habla español";
 }
 
+// Switch
+$dia2 = 2;
+
+switch ($dia) {
+    case 1:
+        echo "lunes";
+        break;
+    case 2:
+        echo "martes";
+        break;
+    case 3:
+        echo "miercoles";
+        break;
+    case 4:
+        echo "jueves";
+        break;
+    case 5:
+        echo "viernes";
+        break;
+    
+    default:
+        echo 'es fin de semana';
+        break;
+}
+
 ?>
