02bd4b7dc9ca1eb87ee2505c12b1efb02fb99a5f
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-24 22:45:53 -0600

    Variables GET

---
+++ 06-variables-superglobales/formulario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Formulario en PHP</h1>
+    <form action="recibir.php" method="get">
+        <p><label for="nombre">Nombre</label>
+        <input type="text" name="nombre"></p>
+        <p><label for="apellidos">Apellido</label>
+        <input type="text" name="apellido"></p>
+        <input type="submit" value="Enviar datos">
+    </form>
+</body>
+</html>
---
+++ 06-variables-superglobales/recibir.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<h1>'. $_GET['nombre'] . '</h1>';
+echo '<h1>'. $_GET['apellido'] . '</h1>';
+
+?>
