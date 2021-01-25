dcf255f48bae74e243ec74bda7857483f6ba5bb7
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-24 23:41:20 -0600

    GoTo

--- 07-condicionales/index.php
+++ 07-condicionales/index.php
@@ -97,4 +97,16 @@ switch ($dia) {
         break;
 }
 
+// GOTO
+
+goto marca;
+
+echo "<h3>Instrucción 1</h3>";
+echo "<h3>Instrucción 2</h3>";
+echo "<h3>Instrucción 3</h3>";
+echo "<h3>Instrucción 4</h3>";
+
+marca:
+    echo "<h3>Me he saltado 4 echos</h3>"
+
 ?>
