949a3d6af0b0549a4634100bd45f08defcd4e90f
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-29 16:16:44 -0600

    Ejemplo con while

--- 08-iteradores/index.php
+++ 08-iteradores/index.php
@@ -6,11 +6,27 @@
  * de instrucciones tantas veces como sea necesario, en base a una condición.
  */
 
- $numero = 0;
+$numero = 0;
 
- while ($numero <= 10) {
-    echo "<h1>". $numero ."</h1>";
-    $numero++;
- }
+//  while ($numero <= 10) {
+//     echo "<h1>". $numero ."</h1>";
+//     $numero++;
+//  }
+
+if(isset($_GET['numero'])) {
+    // Castear a entero
+    $numero = (int) $_GET['numero'];
+} else {
+    $numero = 1;
+}
+
+var_dump($numero);
+echo "<h1>Tabla de multiplicar del número $numero</h1>";
+
+$contador = 0;
+while($contador <= 10) {
+    echo "<p>$numero x $contador = " . ($numero * $contador) . "</p>";
+    $contador++;
+}
 
 ?>
