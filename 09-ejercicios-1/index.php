f49409b1cab562e312a217d4360a34bcdf004579
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-29 17:54:55 -0600

    Ejercicio E1S1

---
+++ 09-ejercicios-1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Ejercicio 1. Crear dos variables "pais" y "continente" y mostrar su valor por pantalla
+ * (imprimir). Poner en un comentario que tipo de dato tienen.
+ */
+
+$pais = "México";
+$continente = "Ámerica";
+
+echo "La variable pais contiene el valor $pais y es de tipo " . gettype($pais) . ".";
+echo "<br/>";
+echo "La variable continente contiene el valor $continente y es de tipo " . gettype($continente) . ".";
