b5040e3031a5fc36dbcbc5a4ecc65f1794c801b6
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-29 15:50:54 -0600

    Bucle while

---
+++ 08-iteradores/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Bucle while
+/**
+ * Estructura de control multilínea que itera o repite la ejecución de una serie 
+ * de instrucciones tantas veces como sea necesario, en base a una condición.
+ */
+
+ $numero = 0;
+
+ while ($numero <= 10) {
+    echo "<h1>". $numero ."</h1>";
+    $numero++;
+ }
+
+?>
