281fc74e4cac41ebd1c6afbd5f9c03fa5a0f2604
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-29 17:58:32 -0600

    Ejercicio E2S1

--- 09-ejercicios-1/index.php
+++ 09-ejercicios-1/index.php
@@ -11,3 +11,14 @@ $continente = "Ámerica";
 echo "La variable pais contiene el valor $pais y es de tipo " . gettype($pais) . ".";
 echo "<br/>";
 echo "La variable continente contiene el valor $continente y es de tipo " . gettype($continente) . ".";
+
+/**
+ * Ejercicio 2. Escribir un script en php que nos muestre por pantalla los números pares
+ * que hay del 0 al 100.
+ */
+
+for ($i=0; $i < 100; $i++) { 
+    if($i % 2 == 0) {
+        echo "<p>${i}</p>";
+    }
+}
