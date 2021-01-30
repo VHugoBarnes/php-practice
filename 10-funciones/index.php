2a1b6c5a4271cd8bede327af0334b3f39103a144
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-29 19:41:31 -0600

    Funciones

---
+++ 10-funciones/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Funciones
+ */
+
+function muestraNombres() {
+    echo "Keko Kaka <br/>";
+    echo "Malia Kaka <br/>";
+    echo "Reba Kaka <br/>";
+    echo "Mochi Kaka <br/>";
+    echo "Nicole Rodriguez <br/>";
+}
+
+muestraNombres();
+
+// Ejemplo 2
+function numero($n) {
+    echo "<p>$n</p>";
+}
+
+numero(2);
