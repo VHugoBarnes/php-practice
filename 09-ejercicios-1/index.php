88b932ee84d20eaee8e2c75a9402ac11331fee7c
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-29 18:26:50 -0600

    Ejercicio E7S1

--- 09-ejercicios-1/index.php
+++ 09-ejercicios-1/index.php
@@ -107,3 +107,25 @@ for($i=1; $i<=10; $i++) {
 
 echo "</tr>";
 echo "</table>";
+
+/**
+ * Ejercicio 7. Hacer un programa que muestre todos los números impares entre dos números 
+ * que nos lleguen por la URL $_GET[]
+ */
+if(isset($_GET['numero1'])&& isset($_GET['numero2'])) {
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+
+    if($numero1 < $numero2) {
+        for($i = $numero1; $i <= $numero2; $i++) {
+            if($i%2 != 0) {
+                echo "<h4>$i</h4>";
+            }
+        }
+    } else {
+        echo "<p>El primer número es más pequeño que el segundo</p>";
+    }
+
+} else {
+    echo "<p>Ingresa los números por la url</p>";
+}
