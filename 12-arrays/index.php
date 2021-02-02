1e60c68de41371d0c0164dc5b65da453a5f564fa
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-01 18:18:35 -0600

    Arrays

---
+++ 12-arrays/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Arrays
+$pelicula = "Joker";
+$peliculas = array("The Pianist", "La la land", "Ladybird", "Inglourious Basterds", "Avengers");
+
+var_dump($peliculas);
+echo "<br>";
+var_dump($peliculas[2]);
+echo "<br>";
+
+$artistas = ['Aurora', 'Kero Kero Bonito', 'The Do', 'Wargasm', 'Sordid Pink'];
+var_dump($artistas);
+echo "<br>";
+var_dump($artistas[4]);
+echo "<br>";
+echo $artistas[4];
