efb658c76c033fa6a884a00b32305f7ca0f86736
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-04 13:10:52 -0600

    Tipos de campos

--- 15-formularios/index.php
+++ 15-formularios/index.php
@@ -14,6 +14,43 @@
 
         <label for="apellido">Apellido</label>
         <input type="text" name="apellido"> <br>
+        
+        <label for="button">Button</label>
+        <input type="button" name="button" value="clikeame"><br>
+        
+        <label for="sexo">Sexo</label>
+        <p>
+            Hombre: <input type="checkbox" name="sexo" value="hombre"/>
+            Mujer: <input type="checkbox" name="sexo" value="mujer"/>
+        </p>
+        
+        <label for="color">Color</label>
+        <input type="color" name="color" value=""><br>
+        
+        <label for="fecha">fecha</label>
+        <input type="date" name="fecha" value=""><br>
+        
+        <label for="correo">correo</label>
+        <input type="email" name="correo" value=""><br>
+        
+        <label for="archivo">archivo</label>
+        <input type="file" name="archivo" value=""><br>
+        
+        <label for="number">number</label>
+        <input type="number" name="number" value=""><br>
+        
+        <label for="password">password</label>
+        <input type="password" name="password" value=""><br>
+        
+        <br>
+        <label for="radiobutton">radiobutton</label> <br>
+        America<input type="radio" name="radiobutton" value="America"><br>
+        Europa<input type="radio" name="radiobutton" value="Europa"><br>
+        Asia<input type="radio" name="radiobutton" value="Asia"><br>
+
+
+        <label for="url">url</label>
+        <input type="url" name="url" value=""><br>
 
         <input type="submit" value="enviar">
         
