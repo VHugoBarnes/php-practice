866bea3d836b7b8f44b5631b510856aae0d3d45d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-03 23:21:22 -0600

    Formularios, campos y atributos

---
+++ 15-formularios/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios PHP y HTML</title>
+</head>
+<body>
+    <h1>Formulario</h1>
+    <form action="" method="POST" enctype="multipart/form-data">
+        
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre"> <br>
+
+        <label for="apellido">Apellido</label>
+        <input type="text" name="apellido"> <br>
+
+        <input type="submit" value="enviar">
+        
+    </form>
+</body>
+</html>
+
+<?php
+
+/**
+ * 
+ */
+
+?>
