72793cbf20b0d0b72f8e2c7d904218a6452788fc
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-05 14:30:40 -0600

    Formulario para validad

---
+++ 16-validacion/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validación de formularios</title>
+</head>
+<body>
+    <h1>Validación de formularios en php</h1>
+
+    <form action="procesar_formulario.php" method="post">
+        <p>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" required="required" pattern="[A-Za-z]+">
+        </p>
+        <p>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos" required pattern="[A-Za-z]+">
+        </p>
+        <p>
+            <label for="edad">Edad</label>
+            <input type="number" name="edad" required pattern="[0-9]+">
+        </p>
+        <p>
+            <label for="email">E-Mail</label>
+            <input type="email" name="email" required>
+        </p>
+        <p>
+            <label for="contrasena">Contraseña</label>
+            <input type="password" name="contrasena" required>
+        </p>
+        <p>
+            <input type="submit" value="enviar">
+        </p>
+    </form>
+
+</body>
+</html>
