3da170f3398b4483e8f07bd72fc32b53e21b84e5
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-06 15:11:37 -0600

    Manipular archivos

--- 17-archivos/index.php
+++ 17-archivos/index.php
@@ -1,18 +1,27 @@
 <?php
 
-// Abrir fichero
-$archivo = fopen('fichero_texto.txt', 'a+');
+// // Abrir fichero
+// $archivo = fopen('fichero_texto.txt', 'a+');
 
-// Leer archivo
-while(!feof($archivo)) {
-    $contenido = fgets($archivo);
-    echo $contenido . "<br>";
-}
+// // Leer archivo
+// while(!feof($archivo)) {
+//     $contenido = fgets($archivo);
+//     echo $contenido . "<br>";
+// }
 
-// Escribir en archivo
-fwrite($archivo, 'Soy un texto metido desde PHP');
+// // Escribir en archivo
+// fwrite($archivo, 'Soy un texto metido desde PHP');
 
-// Cerrar archivo
-fclose($archivo); 
+// // Cerrar archivo
+// fclose($archivo); 
+
+// Copiar fichero
+// copy('fichero_texto.txt', 'fichero_copiado.txt') or die("Error al copiar");
+
+// Renonmbar un fichero
+// rename('fichero_copiado.txt', 'archivo.md');
+
+// Eliminar un fichero
+unlink('archivo.md') or die("Error al borrar");
 
 ?>
