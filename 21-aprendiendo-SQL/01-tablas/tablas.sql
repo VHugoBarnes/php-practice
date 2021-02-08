8180e9f6882082968294a136727c7f036cd05efc
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-08 14:35:15 -0600

    crear tablas

---
+++ 21-aprendiendo-SQL/01-tablas/tablas.sql
@@ -0,0 +1,11 @@
+/*
+ * CREAR TABLA
+ */
+
+CREATE TABLE usuarios(
+    id          int(11),
+    nombre      varchar(100),
+    apellidos   varchar(255),
+    email       varchar(100),
+    password    varchar(255),
+);
