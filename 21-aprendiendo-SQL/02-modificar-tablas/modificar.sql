1896559ed6d32aae29eac794e8dfb9217e32ed10
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-08 15:36:27 -0600

    Modificar columnas

--- 21-aprendiendo-SQL/02-modificar-tablas/modificar.sql
+++ 21-aprendiendo-SQL/02-modificar-tablas/modificar.sql
@@ -7,3 +7,12 @@ ALTER TABLE usuarios_renom CHANGE apellidos apellido varchar(100) null;
 
 # Modificar columna sin cambiar nombre #
 ALTER TABLE usuarios_renom MODIFY apellido char(40) not null;
+
+# Añadir columna #
+ALTER TABLE usuarios_renom ADD website varchar(100) null;
+
+# Añadir restriccion a columna #
+ALTER TABLE usuarios_renom ADD CONSTRAINT uq_email UNIQUE(email);
+
+# Borrar una columna #
+ALTER TABLE usuarios_renom DROP website; 
