1c95485387c5fec894a5f77838874aaa0ef68aa9
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-11 12:50:37 -0600

    Insert básico

---
+++ 21-aprendiendo-SQL/04-DML/insert.sql
@@ -0,0 +1,4 @@
+# Insertar datos #
+INSERT INTO usuarios VALUES(null, 'Victor', 'Vázquez', 'hugo@mail.com', 'prueba', '2021-02-10');
+INSERT INTO usuarios VALUES(null, 'Malia', 'Kaka', 'malia@mail.com', 'prueba', '2021-02-10');
+INSERT INTO usuarios VALUES(null, 'Keko', 'Kaka', 'keko@mail.com', 'prueba', '2021-02-10');
