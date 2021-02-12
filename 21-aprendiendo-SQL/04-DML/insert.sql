c64f286cb0e64da2fc9db076b17a6d4932611f10
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-11 21:35:18 -0600

    Select básico

--- 21-aprendiendo-SQL/04-DML/insert.sql
+++ 21-aprendiendo-SQL/04-DML/insert.sql
@@ -2,3 +2,7 @@
 INSERT INTO usuarios VALUES(null, 'Victor', 'Vázquez', 'hugo@mail.com', 'prueba', '2021-02-10');
 INSERT INTO usuarios VALUES(null, 'Malia', 'Kaka', 'malia@mail.com', 'prueba', '2021-02-10');
 INSERT INTO usuarios VALUES(null, 'Keko', 'Kaka', 'keko@mail.com', 'prueba', '2021-02-10');
+
+# Insertar filas solo con ciertas columnas #
+INSERT INTO usuarios(id, nombre, email, password, fecha) VALUES(null, 'Admin','admin@admin.com', 'pass', '2021-02-09');
+
---
+++ 21-aprendiendo-SQL/04-DML/select.sql
@@ -0,0 +1,5 @@
+-- Mostrar todos los registros / filas de una tabla
+SELECT email, nombre, apellidos FROM usuarios;
+
+-- Mostrar todos los campos
+SELECT * FROM usuarios;
