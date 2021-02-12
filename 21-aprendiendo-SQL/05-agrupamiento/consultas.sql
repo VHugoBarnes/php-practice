9a5297133b2241ac4f95a67dce5ca5c17d431ecf
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-12 15:31:17 -0600

    Condición having

--- 21-aprendiendo-SQL/05-agrupamiento/consultas.sql
+++ 21-aprendiendo-SQL/05-agrupamiento/consultas.sql
@@ -1,3 +1,6 @@
 -- Consultas de agrupamiento
 SELECT titulo FROM entradas GROUP BY categoria_id;
 SELECT COUNT(titulo) AS 'Numero entradas', categoria_id FROM entradas GROUP BY categoria_id;
+
+-- Consultas de agrupamiento con condiciones
+SELECT COUNT(titulo) AS 'Numero entradas', categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(titulo) >= 10;
