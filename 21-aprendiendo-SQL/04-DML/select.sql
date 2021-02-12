79b0ca45a83e7dcc2738780e89fbc79706903fb5
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-11 21:39:52 -0600

    Operadores aritméticos en SQL

--- 21-aprendiendo-SQL/04-DML/select.sql
+++ 21-aprendiendo-SQL/04-DML/select.sql
@@ -3,3 +3,10 @@ SELECT email, nombre, apellidos FROM usuarios;
 
 -- Mostrar todos los campos
 SELECT * FROM usuarios;
+
+-- Operadores aritmeticos
+SELECT email, (4+7) AS operacion FROM usuarios;
+
+-- Operador de ordenamiento
+SELECT id, email, (4+7) AS operacion FROM usuarios ORDER BY email;
+
