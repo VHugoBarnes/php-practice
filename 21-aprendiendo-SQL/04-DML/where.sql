b7441d196436cec971f80884641c6ef1c363cf0d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-12 13:18:36 -0600

    Ejemplos consultas SELECT

--- 21-aprendiendo-SQL/04-DML/where.sql
+++ 21-aprendiendo-SQL/04-DML/where.sql
@@ -34,3 +34,10 @@ SELECT nombre, apellidos FROM usuarios WHERE DAY(fecha) != 09;
 -- Mostrar el email de los usuarios cuyo apellido contenga la letra A
 -- y que la contraseña sea 'prueba'
 SELECT email FROM usuarios WHERE apellidos LIKE '%a%' AND password = 'prueba';
+
+-- Sacar todos los registros de la tabla usuarios cuyo año sea par
+SELECT * FROM usuarios WHERE (YEAR(fecha)%2 = 0);
+
+-- Sacar todos los registros de la tabla usuarios cuyo nombre tenga más de 4 letras
+-- y que se hayan registrado antes del 2022, y mostrar el nombre en mayúsculas
+SELECT *, UPPER(nombre) AS nombre FROM usuarios WHERE LENGTH(nombre) > 4 AND YEAR(fecha) < 2022;
