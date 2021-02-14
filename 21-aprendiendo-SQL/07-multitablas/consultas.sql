20b1a7565db631145a214dd99a1d03c03b5a4a5f
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-14 15:25:14 -0600

    Ejemplos con consultas multitabla

--- 21-aprendiendo-SQL/07-multitablas/consultas.sql
+++ 21-aprendiendo-SQL/07-multitablas/consultas.sql
@@ -5,3 +5,15 @@
 SELECT u.nombre AS 'Usuario', e.titulo, c.nombre AS 'Categoria'
 FROM entradas e, usuarios u , categorias c
 WHERE e.usuario_id = u.id AND e.categoria_id = c.id;
+
+-- Mostrar el nombre de las categorías y de lado cuántas entradas tienen
+SELECT c.nombre AS 'Categorias', COUNT(e.id)
+FROM categorias c, entradas e
+WHERE e.categoria_id = c.id
+GROUP BY e.categoria_id;
+
+-- Mostrar el email de los usuarios y de lado cuántas entradas tienen
+SELECT u.email, COUNT(e.id) AS "N° de entradas"
+FROM usuarios u, entradas e
+WHERE e.usuario_id = u.id
+GROUP BY e.usuario_id;
