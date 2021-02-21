5d8885173cba993f6078a487faaba6c93618088c
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-21 14:49:53 -0600

    Ejercicio 23 SQL

---
+++ 21-aprendiendo-SQL/09-ejercicios/ejercicio23.sql
@@ -0,0 +1,7 @@
+-- Ejercicio 23. Listar todos los encargos realizados con la marca
+-- del coche y el nombre del cliente
+
+SELECT e.*, c.marca, c.modelo, cl.nombre
+FROM encargos e
+INNER JOIN coches c ON c.id = e.coche_id
+INNER JOIN clientes cl ON cl.id = e.cliente_id;
