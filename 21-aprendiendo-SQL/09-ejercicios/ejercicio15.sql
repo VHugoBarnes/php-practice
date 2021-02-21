d8a88b047cbd495a23291b883eb811480939073e
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-20 19:05:15 -0600

    Ejercicio 15 SQL

---
+++ 21-aprendiendo-SQL/09-ejercicios/ejercicio15.sql
@@ -0,0 +1,8 @@
+-- Ejercicio 15. Mostrar los clientes que más pedidos han hecho
+-- y mostrar cuántos hicieron.
+
+SELECT c.nombre, COUNT(e.id)
+FROM encargos e
+INNER JOIN clientes c ON c.id = e.cliente_id
+GROUP BY cliente_id
+ORDER BY 2 DESC LIMIT 2;
