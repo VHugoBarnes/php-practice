a72e64c9bef03a09725b7617db91f49b14d0b312
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-21 15:11:47 -0600

    Ejercicio 25 SQL

---
+++ 21-aprendiendo-SQL/09-ejercicios/ejercicio25.sql
@@ -0,0 +1,14 @@
+-- Ejercicio 25. Obtener una lista de los nombres de los clientes
+-- con el importe de sus encargos acumulado
+
+SELECT cl.nombre, SUM(e.cantidad * co.precio)
+FROM encargos e
+INNER JOIN clientes cl ON cl.id = e.cliente_id
+INNER JOIN coches co ON co.id = e.coche_id;
+
+SELECT ci.nombre, SUM(precio*cantidad) AS 'Importe'
+FROM clientes ci
+INNER JOIN encargos en ON ci.id = en.cliente_id
+INNER JOIN coches co ON en.coche_id = co.id
+GROUP BY ci.nombre
+ORDER BY 2 ASC;
