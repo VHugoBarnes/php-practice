d0592b84411184e43997b8864585fda164a3b403
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-27 17:47:04 -0600

    HTML del menú de navegación

--- 23-blog-php/index.php
+++ 23-blog-php/index.php
@@ -9,15 +9,27 @@
 <body>
     
     <!-- Cabecera -->
-    <header>
+    <header id="header">
+        <!-- Logo -->
         <div id="logo">
             <a href="index.php">
                 Blog de Videojuegos
             </a>
         </div>
+        <!-- Menu -->
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="index.php">Categoría 1</a></li>
+                <li><a href="index.php">Categoría 2</a></li>
+                <li><a href="index.php">Categoría 3</a></li>
+                <li><a href="index.php">Categoría 4</a></li>
+                <li><a href="index.php">Sobre mí</a></li>
+                <li><a href="index.php">Contacto</a></li>
+            </ul>
+        </nav>
     </header>
 
-    <!-- Menu -->
 
     <!-- Barra lateral -->
 
