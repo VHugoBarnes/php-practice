4463b99b5354dadbf291d86d25f20cbb297e67ae
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-27 17:41:27 -0600

    HTML de la cabecera

---
+++ 23-blog-php/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    
+    <!-- Cabecera -->
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                Blog de Videojuegos
+            </a>
+        </div>
+    </header>
+
+    <!-- Menu -->
+
+    <!-- Barra lateral -->
+
+    <!-- Caja principal -->
+
+    <!-- Pie de página -->
+
+</body>
+</html>
