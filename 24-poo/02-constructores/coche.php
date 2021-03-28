a6c72793ad98be21ad2995006f247c6ce3e4c9f5
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-03-27 21:42:19 -0500

    Constructor

---
+++ 24-poo/02-constructores/coche.php
@@ -0,0 +1,51 @@
+<?php
+
+class Coche {
+
+    // Atributos
+    public $color;
+    public $marca;
+    public $modelo;
+    public $velocidad;
+    public $caballaje;
+    public $plazas;
+
+    public function __construct($color, $marca, $modelo, $velocidad, $caballaje, $plazas) {
+        $this -> color = $color;
+        $this -> marca = $marca;
+        $this -> modelo = $modelo;
+        $this -> velocidad = $velocidad;
+        $this -> caballaje = $caballaje;
+        $this -> plazas = $plazas;
+    }
+
+    // Métodos
+    public function getColor() {
+        return $this -> color;
+    }
+
+    public function setColor($c) {
+        $this -> color = $c;
+    }
+
+    public function acelerar() {
+        $this ->  velocidad++;
+    }
+
+    public function frenar() {
+        $this -> velocidad--;
+    }
+
+    public function getVelocidad() {
+        return $this -> velocidad;
+    }
+
+    public function setModelo($modelo) {
+        $this -> modelo = $modelo;
+    }
+
+    public function getModelo() {
+        return $this -> modelo;
+    }
+
+}
---
+++ 24-poo/02-constructores/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'coche.php';
+
+$coche = new Coche("Negro", "Tesla", "Model S", 1000, 1000, 8);
+
+echo "<pre> " , var_export($coche) , " </pre>";
+die();
