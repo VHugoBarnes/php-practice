e10ee949b280aff0339ca19b513eda8e8595a4d4
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-03-30 11:45:45 -0500

    Generar archivos PDF personzalizados

---
+++ 26-librerias/generar-pdf/pdf_para_generar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Keko</title>
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+            color: #3f3f3f;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola mundo desde una librería de PHP para hacer PDFs</h1>
+    <p>Este es un master en PHP</p>
+    <ul>
+       <li>Curso react</li> 
+       <li>Curso node</li> 
+       <li>Curso php</li> 
+    </ul>
+</body>
+</html>
