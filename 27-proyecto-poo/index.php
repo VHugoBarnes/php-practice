09714b91f6546e8beca5931697fb85cf790bf01e
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-03-30 18:05:10 -0500

    Maquetacion de la barra lateral tienda

--- 27-proyecto-poo/index.php
+++ 27-proyecto-poo/index.php
@@ -51,6 +51,7 @@
             <!-- Barra lateral -->
             <aside id="lateral">
                 <div id="login" class="block_aside">
+                    <h3>Entrar a la web</h3>
                     <form action="#" method="POST">
                         <label for="email">Email</label>
                         <input type="email" name="email" id="email">
@@ -58,9 +59,11 @@
                         <input type="password" name="password" id="password">
                         <input type="button" value="Enviar">
                     </form>
-                    <a href="#">Mis pedidos</a>
-                    <a href="#">Gestionar pedidos</a>
-                    <a href="#">Gestionar categorias</a>
+                    <ul>
+                        <li><a href="#">Mis pedidos</a></li>
+                        <li><a href="#">Gestionar pedidos</a></li>
+                        <li><a href="#">Gestionar categorias</a></li>
+                    </ul>
                 </div>
             </aside>
 
