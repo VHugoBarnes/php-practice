b6834dcca2937e3e70928fc772c6d3475db4ca7f
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-03-31 14:55:36 -0500

    Guardar categorias de la tienda

--- 27-proyecto-poo/controllers/categoriaController.php
+++ 27-proyecto-poo/controllers/categoriaController.php
@@ -5,11 +5,30 @@ require_once 'models/Categoria.php';
 class categoriaController {
 
     public function index() {
-
+        Utils::isAdmin();
         $categoria = new Categoria();
         $categorias = $categoria->getAll();
 
         require_once 'views/categoria/index.php';
+    }
+
+    public function crear() {
+        Utils::isAdmin();
+        require_once 'views/categoria/crear.php';
+
+    }
+
+    public function save() {
+        Utils::isAdmin();
+
+        if( isset($_POST) && isset($_POST['nombre']) ) {
+            // Guardar la categoria
+            $categoria = new Categoria();
+            $categoria->setNombre($_POST['nombre']);
+            $categoria->save();
+        }
+
+        header("Location: ".base_url."categoria/index");
     }
 
 }
--- 27-proyecto-poo/helpers/utils.php
+++ 27-proyecto-poo/helpers/utils.php
@@ -13,4 +13,12 @@ class Utils {
 
     }
 
+    public static function isAdmin() {
+        if( !isset($_SESSION['admin']) ) {
+            header("Location: ".base_url);
+        } else {
+            return true;
+        }
+    }
+
 }
