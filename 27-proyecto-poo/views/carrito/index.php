33a1cd961967fc59a0e264e15e4674d812f95007
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-01 15:46:28 -0500

    Estadisticas del carrito

--- 27-proyecto-poo/views/carrito/index.php
+++ 27-proyecto-poo/views/carrito/index.php
@@ -7,12 +7,18 @@
         <th>Precio</th>
         <th>Unidades</th>
     </tr>
-    <?php foreach($carrito as $indice => $producto): ?>
-    <tr>
-        <td><img class="thumb" src="<?=$producto['producto']->imagen ? base_url.'uploads/images/'.$producto['producto']->imagen : base_url.'assets/img/camiseta.png' ?>" alt=""></td>
-        <td><a href="<?=base_url?>producto/ver&id=<?=$producto['producto']->id?>"><?=$producto['producto']->nombre?></a></td>
-        <td>$<?=$producto['precio']?></td>
-        <td><?=$producto['unidades']?></td>
-    </tr>
+    <?php foreach ($carrito as $indice => $producto) : ?>
+        <tr>
+            <td><img class="thumb" src="<?= $producto['producto']->imagen ? base_url . 'uploads/images/' . $producto['producto']->imagen : base_url . 'assets/img/camiseta.png' ?>" alt=""></td>
+            <td><a href="<?= base_url ?>producto/ver&id=<?= $producto['producto']->id ?>"><?= $producto['producto']->nombre ?></a></td>
+            <td>$<?= $producto['precio'] ?></td>
+            <td><?= $producto['unidades'] ?></td>
+        </tr>
     <?php endforeach; ?>
 </table>
+
+<div class="total-carrito">
+    <?php $stats = Utils::statsCarrito(); ?>
+    <h3 id="precio-total">Precio total: $<?= $stats['total'] ?></h3>
+    <a href="" class="button button-pedido">Continuar con el pedido</a>
+</div>
--- 27-proyecto-poo/views/categoria/ver.php
+++ 27-proyecto-poo/views/categoria/ver.php
@@ -9,8 +9,8 @@
                     <img src="<?= $producto->imagen ? base_url . 'uploads/images/' . $producto->imagen : base_url . 'assets/img/camiseta.png' ?>" alt="Producto">
                     <h2><?= $producto->nombre ?></h2>
                 </a>
-                <p><?= $productos->precio ?></p>
-                <a href="#" class="button">Comprar</a>
+                <p><?= $producto->precio ?></p>
+                <a href="<?=base_url?>carrito/add&id=<?=$producto->id?>" class="button">Comprar</a>
             </div>
         <?php endwhile; ?>
     <?php endif; ?>
