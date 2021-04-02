4eb6477c75e75344fec4bf3b523b051b1f48ad0a
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-01 20:32:20 -0500

    Cambiar estado de los pedidos

--- 27-proyecto-poo/views/pedido/mis_pedidos.php
+++ 27-proyecto-poo/views/pedido/mis_pedidos.php
@@ -1,4 +1,8 @@
-<h1>Mis pedidos</h1>
+<?php if (isset($gestion)): ?>
+	<h1>Gestionar pedidos</h1>
+<?php else: ?>
+	<h1>Mis pedidos</h1>
+<?php endif; ?>
 
 <table>
     <tr>
@@ -12,6 +16,7 @@
             <td><a href="<?=base_url?>pedido/detalle&id=<?=$pedido->id?>"><?=$pedido->id?></a></td>
             <td>$<?=$pedido->coste?></td>
             <td><?=$pedido->fecha?></td>
+            <td><?=Utils::showStatus($ped->estado)?></td>
         </tr>
     <?php endwhile; ?>
 </table>
