981a702da453994824e0ead07eb8b1dfede2bbb8
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-03 11:54:10 -0500

    Rutas en laravel

---
+++ aprendiendoLaravel/resources/views/mostrar-fecha.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        * {
+            font-family: Helvetica, sans-serif;
+            color: #383838;
+        }
+    </style>
+    <title>Mostrar la fecha</title>
+</head>
+<body>
+    <h1>Fecha actual</h1>
+    <p><?=$fecha?></p>
+</body>
+</html>
--- aprendiendoLaravel/routes/web.php
+++ aprendiendoLaravel/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mostrar-fecha', function () {
+    date_default_timezone_set("America/Matamoros");
+    $fecha = date('d-m-y');
+    return view('mostrar-fecha', array(
+        'fecha' => $fecha
+    ));
+});
