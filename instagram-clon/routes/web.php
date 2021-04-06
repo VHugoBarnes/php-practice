35a2dcfb8b13287a661b2b5a15f6aeb1eaec9c49
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-06 17:32:16 -0500

    Login y registro de usuarios

--- instagram-clon/routes/web.php
+++ instagram-clon/routes/web.php
@@ -11,29 +11,15 @@
 |
 */
 
-use App\Image;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $images = Image::all();
+// Route::get('/', function () {
+    
 
-    foreach ($images as $key => $image) {
-        echo "<pre>" . $image->image_path . "</pre>";
-        echo "<pre>" . $image->description . "</pre>";
-        echo "<pre>" . $image->user->name . ' ' . $image->user->surname . "</pre>";
+//     return view('welcome');
+// });
 
-        if (count($image->comments) > 0) {
-            echo "<h4>Comentarios</h4>";
-            foreach ($image->comments as $comment) {
-                echo "<pre>" . $comment->user->nick . ' - ' . $comment->content . "</pre>";
-            }
-        }
+Auth::routes();
 
-        echo "<h4>Likes: ". count($image->likes)."</h4>";
-
-        echo "<hr>";
-    }
-    die();
-
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
