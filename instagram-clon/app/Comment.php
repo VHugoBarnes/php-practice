7acc890b32ac904d9f3aaddb7cdefdfdf1eaff2d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-05 19:23:59 -0500

    Configurar entidades y relaciones

--- instagram-clon/app/Comment.php
+++ instagram-clon/app/Comment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+
+    // Relación many-to-one
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    // Relación many-to-one
+    public function image() {
+        return $this->belongsTo('App\Image', 'image_id');
+    }
+
 }
--- instagram-clon/app/Image.php
+++ instagram-clon/app/Image.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $table = 'images';
+
+    // Relación one-to-many
+    public function comments() {
+        return $this->hasMany('App\Comment');
+    }
+
+    // Relación one-to-many
+    public function likes() {
+        return $this->hasMany('App\Like');
+    }
+
+    // Relación many-to-one
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
 }
