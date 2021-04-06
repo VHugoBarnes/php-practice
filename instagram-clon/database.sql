676150afe7f6758822752802725f455eb87fa132
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-06 16:17:48 -0500

    Rellenar la base de datos

--- instagram-clon/database.sql
+++ instagram-clon/database.sql
@@ -18,6 +18,10 @@ CREATE TABLE IF NOT EXISTS users(
     CONSTRAINT pk_users PRIMARY KEY(id)
 )ENGINE=InnoDB;
 
+INSERT INTO users VALUES(null, 'user', 'Víctor', 'Vázquez', 'vhugobarnes', 'hugovg799@gmail.com', null, 'test', CURTIME(), CURTIME(), null);
+INSERT INTO users VALUES(null, 'user', 'Keko', 'Kaka', 'kekokaka', 'keko@gmail.com', null, 'test', CURTIME(), CURTIME(), null);
+INSERT INTO users VALUES(null, 'user', 'Malia', 'Kaka', 'maliakaka', 'malia@gmail.com', null, 'test', CURTIME(), CURTIME(), null);
+
 CREATE TABLE IF NOT EXISTS images(
     id              int(255) auto_increment not null,
     user_id         int(255),
@@ -29,6 +33,18 @@ CREATE TABLE IF NOT EXISTS images(
     CONSTRAINT pk_images PRIMARY KEY(id),
     CONSTRAINT fk_images_users FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDB;
+
+INSERT INTO images VALUES(Null, 1, 'test.jpg', 'descripcion de prueba 1', CURTIME(), CURTIME());
+INSERT INTO images VALUES(Null, 1, 'gato.jpg', 'descripcion de prueba 2', CURTIME(), CURTIME());
+INSERT INTO images VALUES(Null, 1, 'perro.jpg', 'descripcion de prueba 3', CURTIME(), CURTIME());
+
+INSERT INTO images VALUES(Null, 2, 'persona.jpg', 'descripcion de prueba 4', CURTIME(), CURTIME());
+INSERT INTO images VALUES(Null, 2, 'auto.jpg', 'descripcion de prueba 5', CURTIME(), CURTIME());
+INSERT INTO images VALUES(Null, 2, 'paisaje.jpg', 'descripcion de prueba 6', CURTIME(), CURTIME());
+
+INSERT INTO images VALUES(Null, 3, 'comida.jpg', 'descripcion de prueba 7', CURTIME(), CURTIME());
+INSERT INTO images VALUES(Null, 3, 'cafe.jpg', 'descripcion de prueba 7', CURTIME(), CURTIME());
+INSERT INTO images VALUES(Null, 3, 'juego.jpg', 'descripcion de prueba 7', CURTIME(), CURTIME());
 
 CREATE TABLE IF NOT EXISTS comments(
     id          int(255) auto_increment not null,
@@ -43,6 +59,11 @@ CREATE TABLE IF NOT EXISTS comments(
     CONSTRAINT fk_comments_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDB;
 
+INSERT INTO comments VALUES(Null, 1, 4, 'Comentario 1', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(Null, 2, 1, 'Comentario 2', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(Null, 3, 2, 'Comentario 3', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(Null, 3, 3, 'Comentario 4', CURTIME(), CURTIME());
+
 CREATE TABLE IF NOT EXISTS likes(
     id          int(255) auto_increment not null,
     user_id     int(255),
@@ -54,3 +75,8 @@ CREATE TABLE IF NOT EXISTS likes(
     CONSTRAINT fk_likes_users FOREIGN KEY(user_id) REFERENCES users(id),
     CONSTRAINT fk_likes_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDB;
+
+INSERT INTO likes VALUES(Null, 1, 1, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(Null, 2, 2, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(Null, 3, 2, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(Null, 2, 2, CURTIME(), CURTIME());
