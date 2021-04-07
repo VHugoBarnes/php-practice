d7afd5ddb2f818fc1321c884dbeb2302995b4410
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-06 22:32:51 -0500

    Recibir los datos del formulario de configuración

--- instagram-clon/app/Http/Controllers/UserController.php
+++ instagram-clon/app/Http/Controllers/UserController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
-    
+
     public function config() {
         return view('user.config');
     }
 
+    public function update(Request $request) {
+        $id      = \Auth::user()->id;
+        $name    = $request->input('name');
+        $surname = $request->input('surname');
+        $nick    = $request->input('nick');
+        $email   = $request->input('email');
+
+        var_dump($id);
+        var_dump($name);
+        var_dump($surname);
+        var_dump($nick);
+        var_dump($email);
+        die();
+    }
 }
