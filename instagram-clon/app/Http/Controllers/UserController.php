4208a09adb602d60d5ac4a887ab4a695879c2120
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-06 23:00:28 -0500

    Actualizar el usuario

--- instagram-clon/app/Http/Controllers/UserController.php
+++ instagram-clon/app/Http/Controllers/UserController.php
@@ -15,18 +15,34 @@ class UserController extends Controller {
     }
 
     public function update(Request $request) {
-        $id      = \Auth::user()->id;
+        // Conseguir el usuario identificado
+        $user    = \Auth::user();
+        $id      = $user->id;
 
+        // Validación del formulario
         $validate = $this->validate($request, [
             'name' => 'required|string|max:255',
             'surname' => 'required|string|max:255',
-            'nick' => 'required|string|max:255|unique:users,nick,'.$id,
-            'email' => 'required|string|email|max:255|unique:users,email,'.$id
+            'nick' => 'required|string|max:255|unique:users,nick,' . $id,
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id
         ]);
 
+        // Recoger los datos del formulario
         $name    = $request->input('name');
         $surname = $request->input('surname');
         $nick    = $request->input('nick');
         $email   = $request->input('email');
+
+        // Asignar nuevos valores al objeto del usuario
+        $user->name    = $name;
+        $user->surname = $surname;
+        $user->nick    = $nick;
+        $user->email   = $email;
+
+        // Ejecutar consulta y cambios en la base de datos
+        $user->update();
+
+        return redirect()->route('config')
+                         ->with(['message'=>'Usuario actualizado correctamente']);
     }
 }
