820c6cba1dc90b4847058d4232fd3e5452471e3d
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-08 14:49:30 -0500

    Método dislike

--- instagram-clon/app/Http/Controllers/LikeController.php
+++ instagram-clon/app/Http/Controllers/LikeController.php
@@ -5,30 +5,57 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Like;
 
-class LikeController extends Controller
-{
-    
-    public function __construct()
-    {
+class LikeController extends Controller {
+
+    public function __construct() {
         $this->middleware('auth');
     }
 
-    public function like($image_id)
+    public function like($image_id) {
+        // Recoger datos del usuario y la imagen
+        $user = \Auth::user();
+
+        // Comprobar si el like ya existe
+        $isset_like = Like::where('user_id', $user->id)->where('image_id', $image_id)->count();
+
+        if ($isset_like == 0) {
+            $like = new Like();
+            $like->user_id = $user->id;
+            $like->image_id = (int)$image_id;
+
+            // Guardar
+            $like->save();
+
+            return response()->json([
+                'like' => $like
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'El like ya existe'
+            ]);
+        }
+    }
+
+    public function dislike($image_id) 
     {
         // Recoger datos del usuario y la imagen
         $user = \Auth::user();
 
-        $like = new Like();
-        $like->user_id = $user->id;
-        $like->image_id = (int)$image_id;
+        // Comprobar si el like ya existe
+        $like = Like::where('user_id', $user->id)->where('image_id', $image_id)->first();
 
-        // Guardar
-        $like->save();
+        if ($like) {
+            // Eliminar
+            $like->delete();
+
+            return response()->json([
+                'like' => $like,
+                'message' => 'Has dado dislike correctamente'
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'El like no existe'
+            ]);
+        }
     }
-
-    public function dislike($image_id)
-    {
-
-    }
-
 }
