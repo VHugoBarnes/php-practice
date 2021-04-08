e19dd48aae4c901948754b546422ce823e62c9ba
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-07 21:28:07 -0500

    Listado de las imágenes

--- instagram-clon/app/Http/Controllers/HomeController.php
+++ instagram-clon/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Image;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Obteniendo los datos
+        $images = Image::orderBy('id', 'desc')->get();
+
+        return view('home', [
+            'images' => $images
+        ]);
     }
 }
--- instagram-clon/resources/views/home.blade.php
+++ instagram-clon/resources/views/home.blade.php
@@ -1,24 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @include('includes.message')
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @include('includes.message')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                @foreach ($images as $image)
+                    <div class="card">
+
+                        <div class="card-header pub_image">
+                            @if ($image->user->image)
+                                <div class="container-avatar">
+                                    <img src="{{ route('user.avatar', ['filename' => $image->user->image]) }}" alt="Avatar" class="avatar">
+                                </div>
+                            @endif
+                            <div class="data-user">{{ $image->user->nick }}</div>
                         </div>
-                    @endif
 
-                    You are logged in!
-                </div>
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
-</div>
 @endsection
